367da4faebbaae241fecce26e5ab938d47f53bf1
Christy Jestin <cjj9000@gmail.com>
2023-12-09 17:26:42 -0500

    Make progress on trajectory integration

--- constants.py
+++ constants.py
@@ -4,9 +4,12 @@ NUM_LAYERS = 18 # actual number is 18, but can use a smaller number to speed up 
 NUM_BLOCKS_PER_LAYER = 3
 
 # block dimensions
-BLOCK_WIDTH = 0.025
-BLOCK_LENGTH = 0.075
-BLOCK_HEIGHT = 0.015
+BLOCK_WIDTH = 0.05
+BLOCK_LENGTH = 0.15
+BLOCK_HEIGHT = 0.03
+
+# table dimensions
+TABLE_HEIGHT = 0.03
 
 # helper arrays for center of mass calculations
 BLOCK_POSITIONS = np.array([-1, 0, 1])
@@ -16,3 +19,6 @@ assert BLOCK_POSITIONS.shape == (NUM_BLOCKS_PER_LAYER,)
 
 # tunable parameters
 FRICTION_THRESHOLD = 0.5
+
+# motion parameters
+PUSH_FRACTION = 0.2
